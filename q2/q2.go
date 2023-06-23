823d2058938da3a9cfa604b8400c5929b66496f2
karlosmafra <kadumaframoraes@gmail.com>
2023-06-22 23:47:27 -0300

    q2

--- q2/q2.go
+++ q2/q2.go
@@ -1,9 +1,22 @@
 package q2
 
+import "strings"
+
 //Escreva uma função para encontrar o prefixo comum mais longo entre um array de strings.
 //
 //Se não houver um prefixo comum, retorne uma string vazia "".
 
 func LongestCommonPrefix(strs []string) string {
-	return ""
+	pre := strs[0]
+
+	for s := 1; s < len(strs); s++ {
+		for !strings.HasPrefix(strs[s], pre) {
+			pre = pre[:len(pre)-1]
+			if len(pre) == 0 {
+				break
+			}
+		}
+	}
+
+	return pre
 }
